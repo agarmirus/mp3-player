c8f76df9a0333083acfab02711d22e74991932f7
sam20u745 <sam20u745@student.bmstu.ru>
2021-05-23 12:27:41 +0300

    Changge pins numbers of buttons

--- button_processing.h
+++ button_processing.h
@@ -17,8 +17,8 @@
 #define BUTTON_PRESSED 1
 #define BUTTON_NOT_PRESSED 0
 
-const PROGMEM int BUTTON_LEFT = 5;
-const PROGMEM int BUTTON_SELECT = 7;
+const PROGMEM int BUTTON_LEFT = 4;
+const PROGMEM int BUTTON_SELECT = 5;
 const PROGMEM int BUTTON_RIGHT = 8;
 
 void button_processsing(int *const button);
