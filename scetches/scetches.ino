841fe15391325f02b2d0575ee2a7da9901cb3d0f
tsaa20u831 <tsarevaa@student.bmstu.ru>
2021-05-23 12:04:22 +0300

    Configure buttons pins in main scetch

--- scetches/scetches.ino
+++ scetches/scetches.ino
@@ -1,3 +1,20 @@
-void setup();
+#include "button_processing.h"
+#include "screen.h"
+#include <stdint.h>
+
+
+const PROGMEM int LEFT_BUTTON = 11;
+const PROGMEM int SELECT_BUTTON = 12;
+const PROGMEM int RIGHT_BUTTON = 13;
+const PROGMEM int POWER_BUTTON = 14;
+
+
+void setup()
+{
+    pinMode(LEFT_BUTTON, INPUT);
+    pinMode(SELECT_BUTTON, INPUT);
+    pinMode(RIGHT_BUTTON, INPUT);
+    pinMode(POWER_BUTTON, INPUT);
+}
 
 void loop();
