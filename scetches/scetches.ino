f660a0153db8651a62fa1499dd99b9a474818f08
tsaa20u831 <tsarevaa@student.bmstu.ru>
2021-05-23 12:35:13 +0300

    Correct buttons pins setup

--- scetches/scetches.ino
+++ scetches/scetches.ino
@@ -1,20 +1,12 @@
 #include "button_processing.h"
 #include "screen.h"
-#include <stdint.h>
-
-
-const PROGMEM int LEFT_BUTTON = 11;
-const PROGMEM int SELECT_BUTTON = 12;
-const PROGMEM int RIGHT_BUTTON = 13;
-const PROGMEM int POWER_BUTTON = 14;
 
 
 void setup()
 {
-    pinMode(LEFT_BUTTON, INPUT);
-    pinMode(SELECT_BUTTON, INPUT);
-    pinMode(RIGHT_BUTTON, INPUT);
-    pinMode(POWER_BUTTON, INPUT);
+    pinMode(pgm_read_word(&BUTTON_LEFT), INPUT_PULLUP);
+    pinMode(pgm_read_word(&BUTTON_SELECT), INPUT_PULLUP);
+    pinMode(pgm_read_word(&BUTTON_RIGTH), INPUT_PULLUP);
 }
 
 void loop();
