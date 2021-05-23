f41ec2358a04fdec936ce15238f7b77815d14e12
sam20u745 <sam20u745@student.bmstu.ru>
2021-05-23 12:01:22 +0300

    Add setting pins modes and define constants

--- button_processing.c
+++ button_processing.c
@@ -1,10 +1,25 @@
-// Вызов функции:
-// int button[4];
-// button_processing(button);
-void button_processsing(int *const button)   // Да, пусть всё-таки функция будет принимать на вход массив флажков
+/*
+ * Module, which handles button clicks
+ *
+ * Call example:
+ * int button[4];
+ * button_processing(button);
+ *
+ * Author: Sapozhkov Andrey
+ * Group: IU7-23B
+ */
+
+#include "button_processing.h"
+
+void button_processsing(int *const button)
 {
-    button[0] = digitalRead(D8) == HIGH ? 1 : 0;
-    button[1] = digitalRead(D9) == HIGH ? 1 : 0;
-    button[2] = digitalRead(D10) == HIGH ? 1 : 0;
-    button[3] = digitalRead(D11) == HIGH ? 1 : 0;
+    pinMode(BUTTON_0, INPUT_PULLDOWN);
+    pinMode(BUTTON_1, INPUT_PULLDOWN);
+    pinMode(BUTTON_2, INPUT_PULLDOWN);
+    pinMode(BUTTON_3, INPUT_PULLDOWN);
+
+    button[0] = digitalRead(BUTTON_0) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
+    button[1] = digitalRead(BUTTON_1) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
+    button[2] = digitalRead(BUTTON_2) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
+    button[3] = digitalRead(BUTTON_3) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
 }
--- button_processing.h
+++ button_processing.h
@@ -1,1 +1,27 @@
+/*
+ * Module, which handles button clicks
+ *
+ * Call example:
+ * int button[4];
+ * button_processing(button);
+ *
+ * Author: Sapozhkov Andrey
+ * Group: IU7-23B
+ */
+
+#ifndef BUTTON_PROCESSING_H
+#define BUTTON_PROCESSING_H
+
+#include <Arduino.h>
+
+#define BUTTON_PRESSED 1
+#define BUTTON_NOT_PRESSED 0
+
+#define BUTTON_0 D2
+#define BUTTON_1 D4
+#define BUTTON_2 D5
+#define BUTTON_3 D8
+
 void button_processsing(int *const button);
+
+#endif
