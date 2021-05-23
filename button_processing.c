9d34ed62732a098daa28cdf310ad72fac853cbc6
sam20u745 <sam20u745@student.bmstu.ru>
2021-05-23 12:20:50 +0300

    Delete setting pins modes and change initializing constants

--- button_processing.c
+++ button_processing.c
@@ -13,11 +13,6 @@
 
 void button_processsing(int *const button)
 {
-    pinMode(BUTTON_0, INPUT_PULLDOWN);
-    pinMode(BUTTON_1, INPUT_PULLDOWN);
-    pinMode(BUTTON_2, INPUT_PULLDOWN);
-    pinMode(BUTTON_3, INPUT_PULLDOWN);
-
     button[0] = digitalRead(BUTTON_0) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
     button[1] = digitalRead(BUTTON_1) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
     button[2] = digitalRead(BUTTON_2) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
--- button_processing.h
+++ button_processing.h
@@ -17,10 +17,10 @@
 #define BUTTON_PRESSED 1
 #define BUTTON_NOT_PRESSED 0
 
-#define BUTTON_0 D2
-#define BUTTON_1 D4
-#define BUTTON_2 D5
-#define BUTTON_3 D8
+const int BUTTON_0 = 5;
+const int BUTTON_1 = 7;
+const int BUTTON_2 = 8;
+const int BUTTON_3 = 11;
 
 void button_processsing(int *const button);
 
