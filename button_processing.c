5c3f2f94a9c324159ead4c69a52d316eaa1b661f
sam20u745 <sam20u745@student.bmstu.ru>
2021-05-23 12:44:46 +0300

    Moved constants into progmem

--- button_processing.c
+++ button_processing.c
@@ -13,7 +13,7 @@
 
 void button_processsing(int *const button)
 {
-    button[0] = digitalRead(BUTTON_LEFT) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
-    button[1] = digitalRead(BUTTON_SELECT) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
-    button[2] = digitalRead(BUTTON_RIGHT) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
+    button[0] = digitalRead(pgm_read_int(&BUTTON_LEFT)) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
+    button[1] = digitalRead(pgm_read_int(&BUTTON_SELECT)) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
+    button[2] = digitalRead(pgm_read_int(&BUTTON_RIGHT)) == LOW ? BUTTON_PRESSED : BUTTON_NOT_PRESSED;
 }
