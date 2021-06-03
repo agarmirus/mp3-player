b247160422873a0cd97223d54290eb24186c0ffd
tsaa20u831 <tsarevaa@student.bmstu.ru>
2021-06-03 16:33:47 +0300

    Added event check and DFM initializing

--- scetches/scetches.ino
+++ scetches/scetches.ino
@@ -1,14 +1,34 @@
 #include "button_processing.h"
 #include "screen.h"
+#include "file_handler.h"
+#include "button_processing.h"
+#include "Interface.h"
+
+#define BUTTONS_COUNT 3
+
+#define INDEX_BUTTON_LEFT 0
+#define INDEX_BUTTON_SELECT 1
+#define INDEX_BUTTON_RIGHT 2
 
 
 void setup()
 {
+    // Initializing buttons ports
+    
     pinMode(pgm_read_word(&BUTTON_LEFT), INPUT_PULLUP);
     pinMode(pgm_read_word(&BUTTON_SELECT), INPUT_PULLUP);
     pinMode(pgm_read_word(&BUTTON_RIGTH), INPUT_PULLUP);
 
-    init_screen();
+    init_screen();  // Initializing screen
+    setupDFM();  // Initializing DFMPlayer
+    init_interface(Interface);  // Initializing interface
 }
 
-void loop();
+void loop()
+{
+    int buttons_flags[BUTTONS_COUNT] = {0};
+
+    button_processing(buttons_flags);
+
+    get_event(Interface, buttons_flags);
+}
