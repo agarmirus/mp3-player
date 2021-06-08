b2da880ed0e11ade07cfde26f51920ec0927a0dc
tsaa20u831 <tsarevaa@student.bmstu.ru>
2021-06-08 13:21:40 +0300

    Added events check

--- sketches/sketches.ino
+++ sketches/sketches.ino
@@ -3,19 +3,37 @@
 #include "Interface.h"
 #include "screen.h"
 
-
 #define BUTTONS_COUNT 3
 
 #define INDEX_BUTTON_LEFT 0
 #define INDEX_BUTTON_SELECT 1
 #define INDEX_BUTTON_RIGHT 2
 
-int buttons[BUTTONS_COUNT] = {0};
+// Events
+#define PLAY_BUTTON_PRESSED 1
+#define FORWARD_BUTTON_PRESSED 2
+#define PREVIOUS_BUTTON_PRESSED 3
+#define VOLUME_CHANGED 4
+#define MUTE_TOGGLED 5
+#define BRIGHTNESS_CHANGED 6
+#define INVERT_TOGGLED 7
+#define BLOCK_CHANGED 8
+#define ITEM_CHANGED 9
+#define LEVEL_CHANGED 10
+#define LEVEL_MODE_CHANGED 11
+#define OK_BUTTON_ERROR_PRESSED 12
+
+// States
+#define PLAY  1
+#define PAUSE 2
 
 // #define _INIT_
 
 Interface interface;
 int8_t event;
+
+int buttons[BUTTONS_COUNT] = {0};
+int current_track_number = 0;
 
 void setup(void)
 {
@@ -42,7 +60,23 @@ void loop(void)
     // Get/set variables 
     // Do stuff ...
 
-    
+    if (event == PLAY_BUTTON_PRESSED)
+    {
+        if (interface.state == PLAY)
+        {
+            play(current_track_number);
+        }
+        else if (interface.state == PAUSE)
+        {
+            stop();
+        }
+    }
+    else if (event == PREVIOUS_BUTTON_PRESSED || event == FORWARD_BUTTON_PRESSED)
+    {
+        stop();
+        current_track_number += event == FORWARD_BUTTON_PRESSED ? 1 : -1;
+        play(current_track_number);
+    }
 
     draw_screen_frame(&interface);
     delay(100);
