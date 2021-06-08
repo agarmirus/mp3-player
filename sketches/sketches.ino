2cf788678c0171e9fa4921cc45918d0849303d29
tsaa20u831 <tsarevaa@student.bmstu.ru>
2021-06-08 13:00:31 +0300

    Added buttons init

--- sketches/sketches.ino
+++ sketches/sketches.ino
@@ -1,6 +1,16 @@
-
+#include "button_processing.h"
+#include "file_handler.h"
 #include "Interface.h"
 #include "screen.h"
+
+
+#define BUTTONS_COUNT 3
+
+#define INDEX_BUTTON_LEFT 0
+#define INDEX_BUTTON_SELECT 1
+#define INDEX_BUTTON_RIGHT 2
+
+int buttons[BUTTONS_COUNT] = {0};
 
 // #define _INIT_
 
@@ -13,6 +23,11 @@ void setup(void)
     init_screen_eeprom();
 #endif
 
+    pinMode(pgm_read_word(&BUTTON_LEFT), INPUT_PULLUP);
+    pinMode(pgm_read_word(&BUTTON_SELECT), INPUT_PULLUP);
+    pinMode(pgm_read_word(&BUTTON_RIGTH), INPUT_PULLUP);
+
+    setupDFM();  // Initializing DFMPlayer
     init_interface(&interface);
     init_screen(&interface);
 }
@@ -21,13 +36,14 @@ void loop(void)
 {
     // Get information about buttons
 
-    event = get_event(&interface, /* buttons */);
+    event = get_event(&interface, buttons);
     check_timer(&interface);
 
     // Get/set variables 
     // Do stuff ...
 
+    
+
     draw_screen_frame(&interface);
     delay(100);
 }
-
