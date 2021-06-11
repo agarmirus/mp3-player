18cadfcb0928b47d1341317f2205dc36e365046f
Волков Даниил Вячеславович <harvest@Harvest.local>
2021-06-11 18:54:44 +0300

    changed files according to issues

--- sketches/file_handler.h
+++ sketches/file_handler.h
@@ -2,25 +2,21 @@
 //  file_handler.h
 //  practice
 //
-//  Created by Волков Даниил Вячеславович on 26.05.2021.
+//  Created by Волков Даниил Вячеславович
 //  Copyright © 2021 Волков Даниил Вячеславович. All rights reserved.
 //
 
 #ifndef FILE_HANDLER_H
 #define FILE_HANDLER_H
 
-
-#include "DFMiniMp3.h"
-#include <stdint.h>
-
-void setupDFM();
-
-void waitMilliseconds(const uint16_t msWait);
+void setupDFM(int volume);
 
 void play(const int num);
 
-void stop();
+void stop_play();
 
 void play_needed_track(int file, int track);
+
+void setVolume_play(int volume);
 
 #endif
